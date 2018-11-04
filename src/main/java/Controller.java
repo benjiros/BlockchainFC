74e7204b44d00a7957ccb0457e9492c7ec626c48
Ros <rosbenjamindu01@gmail.com>
2018-11-04 20:15:14 +0100

    First start

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -7,41 +7,206 @@ import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.Transfer;
 import org.web3j.tx.gas.DefaultGasProvider;
 import org.web3j.utils.Convert;
+import smartContracts.Mercato.Mercato;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.concurrent.ExecutionException;
 
 public class Controller {
 
     // Path to ethereum base dir (This account will be debited)
-    private final String LOCATION_SOURCE_ACCOUNT = "/Users/benjiros/Library/Ethereum/Blockchain FC/keystore/UTC--2018-10-24T14-52-40.358632659Z--30c45fea0f173249978e90aa3d76add6222679bf";
+
+    private String Directory = "/Users/benjiros/Library/Ethereum/Blockchain FC/keystore/";
+
+    private final String LOCATION_SOURCE_ACCOUNT = Directory + "UTC--2018-10-26T15-12-10.854285357Z--a35f975da43a9f032e262ccd51a41923290649fe";
 
     // Password of the source account
-    private final String SOURCE_ACCOUNT_PASSWORD = "liverpool";
+    private final String SOURCE_ACCOUNT_PASSWORD = "coinbase";
 
     // Account to sent the ethers to
-    private final String TARGET_ACCOUNT = "616ac3f434f8aae021db944d350bc17ec1b1f59e"; //Account 2 -> arsenal
-
-    // Amount of ether to send in transaction
-    private final int AMOUNT = 42;
+    private final String TARGET_ACCOUNT = "0x616ac3f434f8aae021db944d350bc17ec1b1f59e"; //Account 2 -> arsenal
 
 
-    public void transferProposal() throws IOException, CipherException, TransactionException, InterruptedException, ExecutionException {
+
+    public String MERCATO_ADDRESS = null;
+    // Connect to local node
+    Web3j web3 = Web3j.build(new HttpService());  // defaults to http://localhost:8545/
+
+
+    public void initiate()  throws IOException, CipherException, TransactionException, InterruptedException, ExecutionException {
         // Connect to local node
-        Web3j web3 = Web3j.build(new HttpService());  // defaults to http://localhost:8545/
+        //Web3j web3 = Web3j.build(new HttpService());  // defaults to http://localhost:8545/
 
         // Load credentials for accessing wallet of source account
         Credentials credentials = WalletUtils.loadCredentials(SOURCE_ACCOUNT_PASSWORD, LOCATION_SOURCE_ACCOUNT);
 
         // Transfer specified amount of ether to target account
-        int AMOUNT_TO_SEND_OFFER = 1;
-        Transfer.sendFunds(web3, credentials, TARGET_ACCOUNT, BigDecimal.valueOf(AMOUNT_TO_SEND_OFFER), Convert.Unit.ETHER).sendAsync().get();
+        int AMOUNT_TO_OPEN_MERCATO = 1;
+        Transfer.sendFunds(web3, credentials, TARGET_ACCOUNT, BigDecimal.valueOf(AMOUNT_TO_OPEN_MERCATO), Convert.Unit.ETHER).sendAsync().get();
 
-        //.deploy(web3, credentials, DefaultGasProvider.GAS_PRICE, DefaultGasProvider.GAS_LIMIT).send();
+        Mercato mercato;
+        try {
+       //     mercato = Mercato.deploy(web3, credentials, DefaultGasProvider.GAS_PRICE, DefaultGasProvider.GAS_LIMIT).send();
+            mercato = Mercato.deploy(web3, credentials,DefaultGasProvider.GAS_PRICE, DefaultGasProvider.GAS_LIMIT).send();
+
+            MERCATO_ADDRESS = mercato.getContractAddress();
+            System.out.println("LE MERCATO EST OUVERT");
+            System.out.println("mercato address : "+ MERCATO_ADDRESS);
+
+        } catch (Exception e){
+            System.err.println(e);
+        }
+    }
+
+    public void transferProposal(String clubAccount, String clubPassword, String player, int duration) throws IOException, CipherException, TransactionException, InterruptedException, ExecutionException {
+
+        // Load credentials for accessing wallet of source account
+        Credentials credentials = WalletUtils.loadCredentials(clubPassword,clubAccount);
+
+        Mercato mercato = Mercato.load(MERCATO_ADDRESS,web3, credentials, DefaultGasProvider.GAS_PRICE, DefaultGasProvider.GAS_LIMIT);
+
+        System.out.println("mercato address : "+ MERCATO_ADDRESS);
+
+        try{
+           // System.out.println(mercato.proposeTransferToPlayer(player,BigInteger.valueOf(duration), BigInteger.valueOf(1)).send());
+            mercato.proposeTransferToPlayer(player,BigInteger.valueOf(duration), BigInteger.valueOf(1)).send();
+            System.out.println("Transfer offer done");
+        } catch (Exception e){
+
+            System.err.println(e);
+        }
+
+    }
+
+    public BigInteger getNumberProposalClub(String clubAccount, String clubPassword) throws IOException, CipherException, TransactionException, InterruptedException, ExecutionException {
+
+        Credentials credentials = WalletUtils.loadCredentials(clubPassword,clubAccount);
+
+        Mercato mercato = Mercato.load(MERCATO_ADDRESS,web3, credentials, DefaultGasProvider.GAS_PRICE, DefaultGasProvider.GAS_LIMIT);
+
+        System.out.println("mercato address : "+ MERCATO_ADDRESS);
+        BigInteger nbOfferClub = null;
+        try{
+
+            nbOfferClub = mercato.getTransferProposalNumberClub().send();
+            System.out.println("votre club a reçu : " + nbOfferClub + "offre(s)");
+
+        } catch (Exception e){
+
+            System.err.println(e);
+        }
+        return nbOfferClub;
+
     }
 
 
+    public void registerPlayer(String playerAccount, String playerPassword) throws IOException, CipherException, TransactionException, InterruptedException, ExecutionException {
+
+
+        // Load credentials for accessing wallet of source account
+        Credentials credentials = WalletUtils.loadCredentials(playerPassword, playerAccount);
+        Mercato mercato = Mercato.load(MERCATO_ADDRESS,web3, credentials, DefaultGasProvider.GAS_PRICE, DefaultGasProvider.GAS_LIMIT);
+
+        System.out.println("mercato address : "+ MERCATO_ADDRESS);
+        mercato.registerPlayer();
+    }
+
+    public void registerPlayerToClub(String club, String playerAccount, String playerPassword, BigInteger duration , BigInteger price) throws IOException, CipherException, TransactionException, InterruptedException, ExecutionException {
+
+        // Load credentials for accessing wallet of source account
+        Credentials credentials = WalletUtils.loadCredentials(playerPassword, playerAccount);
+        Mercato mercato = Mercato.load(MERCATO_ADDRESS,web3, credentials, DefaultGasProvider.GAS_PRICE, DefaultGasProvider.GAS_LIMIT);
+        System.out.println("mercato address : "+ MERCATO_ADDRESS);
+
+        try{
+            mercato.registerPlayer(club, duration , price);
+            System.out.println("Player Registered");
+        }
+        catch (Exception e){
+            System.err.println(e);
+        }
+    }
+
+
+    public void employedOrNot (String playerAccount, String playerPassword) throws IOException, CipherException, TransactionException, InterruptedException, ExecutionException {
+
+        // Load credentials for accessing wallet of source account
+        Credentials credentials = WalletUtils.loadCredentials(playerPassword, playerAccount);
+        Mercato mercato = Mercato.load(MERCATO_ADDRESS,web3, credentials, DefaultGasProvider.GAS_PRICE, DefaultGasProvider.GAS_LIMIT);
+        boolean employed;
+        System.out.println("mercato address : "+ MERCATO_ADDRESS);
+
+        try{
+            employed = mercato.getPlayerEmployed().send();
+
+
+            if(employed){
+                System.out.println("You have a Club");
+            }else{
+                System.out.println("No club for you");
+            }
+        }
+
+        catch (Exception e){
+            System.err.println(e);
+        }
+
+    }
+
+
+    public BigInteger getTransferPlayerProposalNumber(String playerAccount, String playerPassword) throws IOException, CipherException, TransactionException, InterruptedException, ExecutionException {
+
+
+        // Load credentials for accessing wallet of source account
+        Credentials credentials = WalletUtils.loadCredentials(playerPassword, playerAccount);
+
+        Mercato mercato = Mercato.load(MERCATO_ADDRESS,web3, credentials, DefaultGasProvider.GAS_PRICE, DefaultGasProvider.GAS_LIMIT);
+        BigInteger nbOffers = null;
+        try{
+            nbOffers = mercato.getTransferProposalNumberPlayer().send();
+            System.out.println("Vous avez reçu " + nbOffers + " offre(s)");
+        } catch (Exception e){
+            System.err.println(e);
+        }
+        return nbOffers;
+    }
+
+
+
+
+    public BigInteger getNumberOfPlayerInClub(String clubAccount, String clubPassword) throws IOException, CipherException, TransactionException, InterruptedException, ExecutionException{
+        Credentials credentials = WalletUtils.loadCredentials( clubPassword, clubAccount);
+
+        Mercato mercato = Mercato.load(MERCATO_ADDRESS,web3, credentials, DefaultGasProvider.GAS_PRICE, DefaultGasProvider.GAS_LIMIT);
+        BigInteger nbPlayer = null;
+        try{
+            nbPlayer = mercato.getClubNumberOfPlayers().send();
+            System.out.println("Votre club a " + nbPlayer + " joueur(s)");
+        } catch (Exception e){
+            System.err.println(e);
+        }
+        return nbPlayer;
+    }
+
+    public String getPlayerCurrentClub(String playerAccount, String playerPassword) throws IOException, CipherException, TransactionException, InterruptedException, ExecutionException {
+
+        Credentials credentials = WalletUtils.loadCredentials(playerPassword, playerAccount);
+
+        Mercato mercato = Mercato.load(MERCATO_ADDRESS,web3, credentials, DefaultGasProvider.GAS_PRICE, DefaultGasProvider.GAS_LIMIT);
+        String club = null;
+        try{
+            club = mercato.getPlayerCurrentClub().send();
+            System.out.println("Votre club est : " + club );
+        } catch (Exception e){
+            System.err.println(e);
+        }
+        return club;
+    }
+
+
+/*
     public void transferEther() throws IOException, CipherException, TransactionException, InterruptedException, ExecutionException {
 
         // Connect to local node
@@ -53,5 +218,5 @@ public class Controller {
         // Transfer specified amount of ether to target account
 
         Transfer.sendFunds(web3, credentialsWallet1, TARGET_ACCOUNT, BigDecimal.valueOf(AMOUNT), Convert.Unit.ETHER).sendAsync().get();
-    }
+    }*/
 }
--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -2,6 +2,7 @@ import org.web3j.crypto.CipherException;
 import org.web3j.protocol.exceptions.TransactionException;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -16,13 +17,40 @@ public class Launcher {
 
     public static void main(String[] args) throws InterruptedException, TransactionException, CipherException, IOException, ExecutionException {
 
-        System.out.println("Trying to transfere some ether using java and web3j...");
+        String ClubA_longName = "/Users/benjiros/Library/Ethereum/Blockchain FC/keystore/UTC--2018-10-24T14-52-40.358632659Z--30c45fea0f173249978e90aa3d76add6222679bf";
+        String ClubA = "0x30c45fea0f173249978e90aa3d76add6222679bf" ;
+        String ClubA_Password = "liverpool" ;
 
-        EtherShifter shifter = new EtherShifter();
-        shifter.transferEther();
 
-        System.out.println("Ethereum transfer complete :-)");
+        String ClubB_longName = "/Users/benjiros/Library/Ethereum/Blockchain FC/keystore/UTC--2018-10-24T14-52-31.168522329Z--616ac3f434f8aae021db944d350bc17ec1b1f59e" ;
+        String ClubB = "616ac3f434f8aae021db944d350bc17ec1b1f59e" ;
+        String ClubB_Password = "arsenal" ;
 
+        String Player1_longName = "/Users/benjiros/Library/Ethereum/Blockchain FC/keystore/UTC--2018-10-24T14-51-03.916128461Z--970f192e8491c839ba957d7dcb286f1d3fb1ccc4";
+        String Player1 = "0x970f192e8491c839ba957d7dcb286f1d3fb1ccc4" ;
+        String Player1_Password = "" ;
+
+        Controller controller = new Controller();
+
+        controller.initiate();
+
+
+    //    controller.registerPlayer("/Users/benjiros/Library/Ethereum/Blockchain FC/keystore/UTC--2018-10-24T14-51-03.916128461Z--970f192e8491c839ba957d7dcb286f1d3fb1ccc4","");
+        BigInteger duration = BigInteger.valueOf(1);
+        BigInteger price = BigInteger.valueOf(2);
+        controller.registerPlayerToClub( ClubA, Player1_longName ,Player1_Password,  duration , price );
+
+
+
+        controller.employedOrNot(Player1_longName,Player1_Password);
+
+
+        controller.getNumberOfPlayerInClub(ClubA_longName, ClubA_Password);
+
+        controller.transferProposal(ClubB_longName, ClubB_Password,Player1, 1);
+
+
+        controller.getTransferPlayerProposalNumber(Player1_longName,Player1_Password);
 
 
     }
